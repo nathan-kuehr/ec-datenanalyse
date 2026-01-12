21675dcdbfc298fe8048846c615acd2a80d7137e
Nathan Kühr <github@kuehrconnect.de>
2026-01-12 15:09:26 +0100

    Implemented improved version of plot result

---
+++ ecvisualization/src/plotresult.py
@@ -0,0 +1,46 @@
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from datetime import datetime
+from .settings import Settings
+import matplotlib.pyplot as plt
+
+import os
+
+class PlotResult:
+    def __init__(self, title: str|None , figure, **kwargs):
+        self.__figure = figure
+        self.__title = title or datetime.now().strftime("%Y-%m-%d_%H:%M:%S_Plot") 
+
+        self.__saved = kwargs.get("noSave", False)
+        self.__settings = Settings(**kwargs)
+
+        # Save References in case of matplotlib shutdown before saving
+        self.__savefigFunc = self.__figure.savefig
+        self.__showfigFunc = self.__figure.show
+        self.__globalShowFigFunc = plt.show
+
+    def save(self) -> None:
+        if not self.__saved:
+            self.__saved = True
+
+            if self.__settings.showOnSave:
+                self.__showfigFunc()
+                self.__globalShowFigFunc(block=True)
+
+
+            for ext in self.__settings.exportFormats:
+                exportPath = os.path.join(self.__settings.outputFolder, f"{self.__title}.{ext}")
+
+                if ext == "png":
+                    self.__savefigFunc(exportPath, dpi=self.__settings.dpi)
+                else:
+                    self.__savefigFunc(exportPath)
+
+    def __enter__(self):
+        return self.__figure, self.__figure.axes
+    
+    def __exit__(self, excType, excVal, excTb):
+        pass
+    
+    def __del__(self):
+        self.save()
