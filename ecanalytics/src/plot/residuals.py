ed70e981e34af6613e14322d96cfbc24ae71fdfc
Nathan Kühr <github@kuehrconnect.de>
2026-03-18 19:01:21 +0100

    residualy.py ruff cleanup

--- ecanalytics/src/plot/residuals.py
+++ ecanalytics/src/plot/residuals.py
@@ -13,7 +13,10 @@ from ..data.experiment import Experiment
 from .plot import *
 from .plotresult import PlotResult
 
-from ..config import RESIDUAL_PLOT_DEFAULT_FIGSIZE, FIGURE_SETTINGS, SNS_LINEPLOT_DEFAULT_SETTINGS
+from ..config import (
+    RESIDUAL_PLOT_DEFAULT_FIGSIZE,
+    FIGURE_SETTINGS,
+)
 
 
 @singledispatch
@@ -62,17 +65,27 @@ def __residuals_data(
 
     return pr
 
+
 @singledispatch
-def residual_distr(data: DataQuality, title: str | None = None, min_bound: float = 1.0, **kwargs) -> PlotResult:
+def residual_distr(
+    data: DataQuality, title: str | None = None, min_bound: float = 1.0, **kwargs
+) -> PlotResult:
     return residual_distr(data.residuals, title, min_bound, **kwargs)
 
+
 @residual_distr.register(pd.DataFrame)
-def __residual_distr_data(data: pd.DataFrame, title: str | None = None, min_bound: float = 1.0, **kwargs) -> PlotResult:
+def __residual_distr_data(
+    data: pd.DataFrame, title: str | None = None, min_bound: float = 1.0, **kwargs
+) -> PlotResult:
     color = data["Palette"].iloc[0].shade(1)[0]
 
-    config = {"x":"Real Residual", "y": "Imag. Residual"}
-    config["joint_kws"] = {"color":color} | kwargs.get("joint_kws", {})
-    config["marginal_kws"] = {"facecolor": to_rgba(color, 0.5), "edgecolor": color, "stat": "density"} | kwargs.get("marginal_kws", {})
+    config = {"x": "Real Residual", "y": "Imag. Residual"}
+    config["joint_kws"] = {"color": color} | kwargs.get("joint_kws", {})
+    config["marginal_kws"] = {
+        "facecolor": to_rgba(color, 0.5),
+        "edgecolor": color,
+        "stat": "density",
+    } | kwargs.get("marginal_kws", {})
 
     residuals = data[["Real Residual", "Imag. Residual"]]
 
@@ -87,7 +100,7 @@ def __residual_distr_data(data: pd.DataFrame, title: str | None = None, min_boun
             fig.suptitle(
                 title,
                 fontsize=FIGURE_SETTINGS["axes.titlesize"],
-                fontweight=FIGURE_SETTINGS["axes.titleweight"]
+                fontweight=FIGURE_SETTINGS["axes.titleweight"],
             )
             fig.subplots_adjust(top=0.94)
 
@@ -101,10 +114,10 @@ def __residual_distr_data(data: pd.DataFrame, title: str | None = None, min_boun
         Series_Info = Experiment.Series_Info | DataQuality.Series_Info
 
         joint_ax.set(
-            xlabel=f"Real Residual {Series_Info["Real Residual"].symbol} [{Series_Info["Real Residual"].unit}]",
-            ylabel=f"Imag. Residual {Series_Info["Imag. Residual"].symbol} [{Series_Info["Imag. Residual"].unit}]",
+            xlabel=f"Real Residual {Series_Info['Real Residual'].symbol} [{Series_Info['Real Residual'].unit}]",
+            ylabel=f"Imag. Residual {Series_Info['Imag. Residual'].symbol} [{Series_Info['Imag. Residual'].unit}]",
             xlim=lims,
-            ylim=lims
+            ylim=lims,
         )
         joint_ax.grid(True, which="both", linestyle="--", alpha=0.4)
         joint.refline(x=0, y=0, linewidth=0.75, marginal=True, zorder=0, linestyle="-.")
@@ -120,14 +133,15 @@ def __residual_distr_data(data: pd.DataFrame, title: str | None = None, min_boun
 
             ax.plot(*gauss_data, color=color, linestyle="--")
 
-
         rms = np.sqrt(2 * np.mean(np.square(residuals.to_numpy())))
-        rho = residuals.corr("pearson").iloc[0,1]
+        rho = residuals.corr("pearson").iloc[0, 1]
 
-        phantom = Rectangle((0,0), 1, 1, visible=False) 
-        joint_ax.legend([phantom] * 2, 
-                        ["$\\Delta_{rms}$" + f" = {rms:.2f} %", f"$\\rho$ = {rho:.4f}"], 
-                        loc="best", 
-                        frameon=False)
+        phantom = Rectangle((0, 0), 1, 1, visible=False)
+        joint_ax.legend(
+            [phantom] * 2,
+            ["$\\Delta_{rms}$" + f" = {rms:.2f} %", f"$\\rho$ = {rho:.4f}"],
+            loc="best",
+            frameon=False,
+        )
 
     return PlotResult(title, fig, **kwargs)
