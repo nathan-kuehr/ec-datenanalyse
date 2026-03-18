b7fc44f7dc7ea35a66b857e30482a548f3691b88
Nathan Kühr <github@kuehrconnect.de>
2026-03-18 18:56:35 +0100

    Überarbeitete Konfiguration

--- ecanalytics/src/config.py
+++ ecanalytics/src/config.py
@@ -1,3 +1,34 @@
+from typing import NamedTuple
+
+
+class DataSeriesInfo(NamedTuple):
+    symbol: str
+    unit: str
+    scale: str
+
+
+EIS_SAMPLE_REQUIRED_DATA_SERIES = {
+    "Frequency",
+    "Impedance",
+    "Resistance",
+    "Neg. Reactance",
+    "Phase",
+}
+
+EIS_EXPERIMENT_SERIES_INFO = {
+    "Frequency": DataSeriesInfo("$F$", "Hz", "log"),
+    "Impedance": DataSeriesInfo("$Z$", "$\\Omega$", "log"),
+    "Capacitance": DataSeriesInfo("$C$", "F", "log"),
+    "Phase": DataSeriesInfo("$\\varphi$", "°", "linear"),
+    "Resistance": DataSeriesInfo("$R$", "$\\Omega$", "log"),
+    "Offset-Corrected Resistance": DataSeriesInfo("$R_{oc}$", "$\\Omega$", "log"),
+    "Neg. Reactance": DataSeriesInfo("-$X$", "$\\Omega$", "log"),
+}
+
+EIS_EXPERIMENT_FREQUENCY_TOLERANCE = (
+    1e-3  # Tolerance for frequency matching in EIS experiments
+)
+
 FIGURE_SETTINGS = {
     "font.family": "Arial",
     "font.size": 12,
@@ -9,15 +40,26 @@ FIGURE_SETTINGS = {
     "legend.title_fontsize": 11,
     "axes.spines.top": False,
     "axes.spines.right": False,
+    "figure.figsize": (7, 6),
 }
 
-DEFAULT_FIGURE_SIZE = (7, 6)
-DEFAULT_LINEWIDTH = 2.5
-DEFAULT_GRID_ALPHA = 0.4
-DEFAULT_MARKER_SIZE = 5
-DEFAULT_MARKER = "o"
+RESIDUAL_PLOT_DEFAULT_FIGSIZE = (13, 6)
+
+SNS_LINEPLOT_DEFAULT_SETTINGS = {
+    "linewidth": 2.5,
+    "marker": "o",
+    "markersize": 5,
+    "markeredgewidth": 0,
+    "errorbar": ("se", 95),
+    "err_style": "band",
+}
 
 COVVIS_ANGLE_STEPS = 1
 COVVIS_INTERPOLATION_POINTS = 30
 
-FREQUENCY_TOLERANCE = 1e-3  # Tolerance for frequency matching in EIS experiments
+DATA_QUALITY_SERIES_INFO = {
+    "Frequency": DataSeriesInfo("$F$", "Hz", "log"),
+    "Real Residual": DataSeriesInfo("$\\Delta_{re}$", "% of |Z|", "linear"),
+    "Imag. Residual": DataSeriesInfo("$\\Delta_{im}$", "% of |Z|", "linear"),
+    "Residual": DataSeriesInfo("$\\Delta$", "% of |Z|", "linear"),
+}
