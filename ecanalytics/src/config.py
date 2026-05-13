5d1d20fac88695748d8d8a69c088a9904ed2c018
Nathan Kühr <github@kuehrconnect.de>
2026-05-13 14:03:45 +0200

    Konfig erweitert für Randles Fitting Parameter

--- ecanalytics/src/config.py
+++ ecanalytics/src/config.py
@@ -16,13 +16,13 @@ EIS_SAMPLE_REQUIRED_DATA_SERIES = {
 }
 
 EIS_EXPERIMENT_SERIES_INFO = {
-    "Frequency": DataSeriesInfo("$F$", "Hz", "log"),
-    "Impedance": DataSeriesInfo("$Z$", "$\\Omega$", "log"),
-    "Capacitance": DataSeriesInfo("$C$", "F", "log"),
-    "Phase": DataSeriesInfo("$\\varphi$", "°", "linear"),
-    "Resistance": DataSeriesInfo("$R$", "$\\Omega$", "log"),
-    "Offset-Corrected Resistance": DataSeriesInfo("$R_{oc}$", "$\\Omega$", "log"),
-    "Neg. Reactance": DataSeriesInfo("-$X$", "$\\Omega$", "log"),
+    "Frequency": DataSeriesInfo("F", "Hz", "log"),
+    "Impedance": DataSeriesInfo("Z", "$\\Omega$", "log"),
+    "Capacitance": DataSeriesInfo("C", "F", "log"),
+    "Phase": DataSeriesInfo("varphi", "°", "linear"),
+    "Resistance": DataSeriesInfo("R", "$\\Omega$", "log"),
+    "Offset-Corrected Resistance": DataSeriesInfo("R_oc", "$\\Omega$", "log"),
+    "Neg. Reactance": DataSeriesInfo("-X", "$\\Omega$", "log"),
 }
 
 EIS_EXPERIMENT_FREQUENCY_TOLERANCE = (
@@ -84,6 +84,64 @@ DATA_QUALITY_SERIES_INFO = {
     "Residual": DataSeriesInfo("$\\Delta$", "% of |Z|", "linear"),
     "Time Constant": DataSeriesInfo("$\\tau$", "s", "log"),
     "Polarization Density": DataSeriesInfo(
-        "$\\gamma_{\\mathrm{log}}$", "$\\Omega$", "linear"
+        "$\\gamma_{\\mathrm{log}}$", r"$\frac{\Omega}{\mathrm{s}}$", "linear"
     ),
+}
+
+# FITTING
+
+PARAMETER_PLOT_FIGURE_SETTINGS = {
+    "figure.constrained_layout.use": False,
+    "axes.titlesize": FIGURE_SETTINGS["legend.title_fontsize"]
+}
+
+DEFAULT_PARAMETER_PLOT_SETTINGS = {
+    "catplot_kws": {
+        "col_wrap": 3,
+        "sharey": False,
+        "showfliers": False,
+        "boxprops": {"alpha": .9},
+        "legend": True,
+        "height": 3.5,
+        "aspect": 1.2,
+    },
+    "stripplot_kws": {
+        "size": 4,
+        "jitter": True
+    }
+}
+
+FITTING_RANDLES_SERIES_INFO = {
+    "Charge Transfer Resistance": DataSeriesInfo("R_ct", r"$\Omega$", "linear"),
+    "Solution Resistance": DataSeriesInfo("R_s", r"$\Omega$", "linear"),
+    "Double-Layer Admittance": DataSeriesInfo("Y_dl", r"$\mu\mathrm{S}\mathrm{s}^n$", "linear"),
+    "Double-Layer Dispersion Factor": DataSeriesInfo("n_dl", None, "linear"),
+    "Warburg Admittance": DataSeriesInfo("Y_diff", r"$\mu\mathrm{S}$", "linear"),
+    "Warburg Time Constant": DataSeriesInfo("B_diff", r"$10^{-6}\cdot\mathrm{s}^n$", "linear"),
+    "Warburg Exponent": DataSeriesInfo("n_diff", None, "linear"),
+}
+
+FITTING_DEFAULT_INITIAL_VALUES = {
+    "randles": {
+        "n_dl": 0.97,
+        "B_diff": 5e-5,
+        "Y_diff": 2.5e-4
+    },
+    "zarc": {
+        "n_zarc": 0.97
+    }
+}
+
+FITTING_TO_UNIT_CONVERSION_MULTIPLIERS = {
+    "Y_dl": 1e6,
+    "B_diff": 1e6,
+    "Y_diff": 1e6,
+}
+
+FITTING_DRT_POLARIZATION_CORRECTION_FACTOR = 1.1
+
+
+DEFAULT_LINEPLOT_GRID_SETTINGS = {
+    "kind": "line",
+    "facet_kws": {"sharex": True, "sharey": True},
 }
