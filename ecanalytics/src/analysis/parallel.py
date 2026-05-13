4eadfe9c18d781bc15cf47ed04215c03c83c946d
Nathan Kühr <github@kuehrconnect.de>
2026-05-13 14:01:37 +0200

    Prüfung ob Prozesse gespawnt werden müssen

--- ecanalytics/src/analysis/parallel.py
+++ ecanalytics/src/analysis/parallel.py
@@ -1,8 +1,10 @@
 import os
+import importlib
 
 from collections.abc import Callable
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from joblib import Memory
+from joblib.memory import MemorizedFunc
 from tqdm import tqdm
 from pyimpspec import set_default_num_procs
 
@@ -12,9 +14,13 @@ Cache = Memory("./lab-analytics-cache", verbose=0)
 # Flag for pyimpspec process use setup
 __Num_Procs_Is_Set_Up = False
 
+def _proxy_caller(method_ids: tuple, *args, **kwargs):
+    module, fname = method_ids
+    func = getattr(importlib.import_module(module), fname)
+    return func(*args, **kwargs)
 
 def multiprocess(
-    method: Callable, input: list, tqdm_note: str = "", args: dict | list[dict] = {}
+    method: MemorizedFunc, input: list, tqdm_note: str = "", args: dict | list[dict] = {}
 ) -> list:
     global __Num_Procs_Is_Set_Up
 
@@ -32,19 +38,36 @@ def multiprocess(
     else:
         listed_args = args
 
-    with ProcessPoolExecutor(max_workers=(3 * (os.cpu_count() or 2) // 4)) as executor:
-        # Get futures of the processed data
-        futures = {
-            executor.submit(method, sample, **arg): i
-            for i, (sample, arg) in enumerate(zip(input, listed_args))
-        }
+    # Prepare list for multiprocessing
+    jobs_to_compute = []
 
-        # Update progress bar as futures are filled
-        for future in tqdm(
-            as_completed(futures),
-            total=len(input),
-            desc=tqdm_note,
-        ):
-            results[futures[future]] = future.result()
+    # Check if cached data exists
+    for i, (sample, arg) in enumerate(zip(input, listed_args)):
+        cached = method.check_call_in_cache(sample, **arg)
+
+        if cached: 
+            # Exists in cache -> just load
+            results[i] = method(sample, **arg)
+        else:
+            # Append to job compute lsit
+            jobs_to_compute.append((i, sample, arg))
+
+    # Need to transform method 
+    method_ids = method.func.__module__, method.func.__name__
+
+    if jobs_to_compute:
+        nprocs = (3 * (os.cpu_count() or 2) // 4)
+
+        with ProcessPoolExecutor(max_workers=nprocs) as ex:
+            # Get futures of the processed data
+            futures = {ex.submit(_proxy_caller, method_ids, sample, **arg): i for i, sample, arg in jobs_to_compute}
+
+            # Update progress bar as futures are filled
+            for future in tqdm(
+                as_completed(futures),
+                total=len(jobs_to_compute),
+                desc=tqdm_note,
+            ):
+                results[futures[future]] = future.result()
 
     return results
