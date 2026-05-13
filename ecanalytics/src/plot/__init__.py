a098dcfa549d16f121a7383ce9db02594bbb6b19
Nathan Kühr <github@kuehrconnect.de>
2026-05-13 14:05:09 +0200

    Namenskonflikt aufgelöst - Funktionen werden nun als solche erkannt

--- ecanalytics/src/plot/__init__.py
+++ ecanalytics/src/plot/__init__.py
@@ -3,14 +3,17 @@ from .nyquist_plot import nyquist
 from .basics import plot, bode, fresponse
 from .plotresult import PlotResult
 from .kkt_plots import residuals, residual_distribution
+from .fitting_plots import fitted_parameters
 
 __all__ = [
-    "drt_plot",
-    "nyquist_plot",
-    "basics",
+    "drt",
+    "nyquist",
+    "plot",
     "bode",
     "fresponse",
     "PlotResult",
     "kkt_plots",
+    "residuals",
     "residual_distribution",
+    "fitted_parameters"
 ]
